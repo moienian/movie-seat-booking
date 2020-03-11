ec707eae3db6d1e6d9239495401e4aaa5589792e
Mohammad Moien <mnmoienian@gmail.com>
2020-03-11 17:33:19 +0330

    add data to localstorage for fetching on load page

--- script.js
+++ script.js
@@ -2,21 +2,50 @@ const container = document.querySelector(".container");
 const movie = document.getElementById("movie");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-let priceValue = movie.value;
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+let ticketPrice = +movie.value;
+
+populateUI();
+updateSeclectedCount();
+
+function SetMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movie.selectedIndex = selectedMovieIndex;
+    ticketPrice = +movie.value;
+  }
+}
 
 function updateSeclectedCount() {
-  const selected = document.querySelectorAll(".row .seat.selected");
-  const selectedLength = selected.length;
-  count.innerText = selectedLength;
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
-  const price = +priceValue;
-  total.innerText = selectedLength * price;
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 movie.addEventListener("change", e => {
-  priceValue = e.target.value;
+  ticketPrice = +e.target.value;
+  SetMovieData(e.target.selectedIndex, e.target.value);
   updateSeclectedCount();
 });
+
 container.addEventListener("click", e => {
   if (
     e.target.classList.contains("seat") &&
